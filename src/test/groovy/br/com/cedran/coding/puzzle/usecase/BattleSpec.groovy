f43e5a0beedc1b901374e0f1c60e3e1413fe5d16
gabrielcedran <gabrielcedran@gmail.com>
2018-01-16 09:42:28 +0100

    Update BattleSpec.groovy

--- src/test/groovy/br/com/cedran/coding/puzzle/usecase/BattleSpec.groovy
+++ src/test/groovy/br/com/cedran/coding/puzzle/usecase/BattleSpec.groovy
@@ -13,7 +13,7 @@ import br.com.cedran.coding.puzzle.usecase.battle.EndBattle
 
 class BattleSpec extends BaseSpec {
 
-    Battle battl
+    Battle battle
     Character character
     Random random
     Monster monster
